88afde3ead47a7da2750288ebfd64de9edb0afd3
cpstrillvt93 <Colin@Colins-MacBook-Air.local>
2017-01-03 23:04:13 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,16 @@
 console.log("JS file is connected to HTML! Woo!")
+
+/*cards*/
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+/*comparison of variables*/
+
+cardOne == cardTwo;
+cardOne == cardFour;
+
+
+
